2fb998886e257ce0133dfd02a12bb6222936f135
Vaibhavi Panchal <vaibhavi2226@gmail.com>
2026-05-23 11:55:42 +0530

    Added PWA support

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  reactStrictMode: true,
-};
+import nextPWA from "next-pwa";
 
-export default nextConfig;
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {};
+
+export default withPWA(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content="#7c3aed" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
