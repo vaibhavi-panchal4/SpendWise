8f22f8a9af9d73635b76801ae3c45ff9f08fdf6f
Vaibhavi Panchal <vaibhavi2226@gmail.com>
2026-05-27 09:23:05 +0530

    corrected padding

--- pages/index.js
+++ pages/index.js
@@ -1122,7 +1122,7 @@ export default function Home() {
 
           .add-section-mobile {
             padding: 1.5rem !important;
-            padding-top: 2rem !important;
+            padding-top: 3.5rem !important;
           }
 
           .add-section-mobile button[title="close"] {
